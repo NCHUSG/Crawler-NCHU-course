c6e3e48f4e3fb5f99a593f89129d9ff2f14930d7
david <davidtnfsh@gmail.com>
2016-01-02 10:37:42 +0000

    make the craqler name correct

---
+++ nchu/CoursePickingHelperCrawler.py
@@ -0,0 +1,22 @@
+import requests, json, os
+
+try:
+    os.chdir('/var/www/html/Python-Crawler/nchu/json')
+    #means cd path
+except:
+    print('path error, the file will be dump into the current directory')
+
+degree = ['U', 'G', 'D', 'N', 'O', 'W']
+
+try:
+	for i in degree:
+		re = requests.get('https://onepiece.nchu.edu.tw/cofsys/plsql/json_for_course?p_career=' + i)
+
+		fname = i + '.json'
+		with open(fname, 'w', encoding='UTF-8') as f:
+			f.write(re.text)
+
+	#print('requests success!!')
+except Exception as e:
+	print('requests fail!!')
+	print(e)
