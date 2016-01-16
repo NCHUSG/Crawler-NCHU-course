40b922252170728e49a046d9718a3688203ad389
david <davidtnfsh@gmail.com>
2016-01-16 22:07:11 +0800

    itsa的爬重沒有完成，有點懶的寫

--- itsa_mail_to _ieliao.py
+++ itsa_mail_to _ieliao.py
@@ -11,7 +11,7 @@ team = {"投資理財有風險屁屁買賣沒保險":["劉冠閎", "沈柏瑋", 
 "sisker":["陳盈達", "范浩翔", "林師漢"],
 "室友好carry隊":["張軒譯"],
 "資工對不對":["洪浩祐", "李家駿", "劉鴻文"],
-"誠如code所說":["張泰瑋", "黃翔宇", "林佑如"]
+"儒儒修不修":["張泰瑋", "黃翔宇", "林佑如"]
 }
 email=[]
 
@@ -19,13 +19,18 @@ try:
 	re = requests.get('http://140.116.82.70/itsa_scoreboard/')
 	d=pq(re.text)
 	ac_number=0
-	for i in d('tr'):	
-		for j in team.keys():
-			if $(i).text() == j:
-				$tar=$(iv).parent()
-				email.append($tar.children(j+' '+$tar.children('td:eq(1)')+' '$tar.children('td:eq(2)'))
-		if len(email) == len(team) :
-			break
+	for i in d('tr'):
+		print(d(i).text())
+		with open('itsa.txt', 'a', encoding='UTF-8') as f:
+			print('in')
+			f.write(d(i).text())
+		# for j in team.keys():			
+		# 	if d(i).text() == j:
+		# 		print(d(i))
+			# 	dtar=d(iv).parent()
+			# 	email.append(dtar.children(j+' '+dtar.children('td:eq(1)')+' 'dtar.children('td:eq(2)'))
+		# if len(email) == len(team) :
+		# 	break
 		
 	# $.each($('td'),function(ik,iv){
 	# 	if($(iv).text()=='投資理財有風險屁屁買賣沒保險'){
@@ -42,21 +47,21 @@ try:
 	# })
 
 
-	fname = i + '.txt'
-		with open(fname, 'a', encoding='UTF-8') as f:
-			f.write('''主任教安：
+	# fname = i + '.txt'
+	# 	with open(fname, 'a', encoding='UTF-8') as f:
+	# 		f.write('''主任教安：
 
-   下列是有參加競賽的學弟
-				''')				
-		with open(fname, 'a', encoding='UTF-8') as f:
-			for i in email:
-				f.write(i)
+ #   下列是有參加競賽的學弟
+	# 			''')				
+	# 	with open(fname, 'a', encoding='UTF-8') as f:
+	# 		for i in email:
+	# 			f.write(i)
 
-		with open(fname, 'a', encoding='UTF-8') as f:
-			f.write('所有人都有解出'+str(ac_number)+'題')
-			f.write()
-			f.write('學生泰瑋敬上')
-	print('email scrpit finished!!')
+	# 	with open(fname, 'a', encoding='UTF-8') as f:
+	# 		f.write('所有人都有解出'+str(ac_number)+'題')
+	# 		f.write()
+	# 		f.write('學生泰瑋敬上')
+	# print('email scrpit finished!!')
 except Exception as e:
 	print('email scrpit fail!!')
 	print(e)
