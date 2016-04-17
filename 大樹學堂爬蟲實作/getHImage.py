591409a4be664acdbfccad6492004db300b2855f
david <davidtnfsh@gmail.com>
2016-04-17 17:47:15 +0800

    新增人性化介面

--- 大樹學堂爬蟲實作/getHImage.py
+++ 大樹學堂爬蟲實作/getHImage.py
@@ -1,18 +1,22 @@
+#!/usr/bin/python3
 import requests, json
 from bs4 import BeautifulSoup
 import shutil
 import os
+
+path = input("Please input the path you want to store these image:")
 try:
-    os.chdir('img')
+    os.chdir(path)
     #means cd path
 except:
     print('path error')
-res =requests.get('http://www.timliao.com/bbs/viewthread.php?tid=74448')
+targetURL = input("Please input the url that you want to Parse:")
+res =requests.get(targetURL)
 soup = BeautifulSoup(res.text)
-
 for i in soup.select('img'):
     try:
         fname = i['src'].split('/')[-1]
+        fname = fname+".jpg"
         imageUrl = i['src']
         ires = requests.get(imageUrl,stream=True)
         f = open(fname,'wb')
